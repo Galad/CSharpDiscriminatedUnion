b967cadad0a44231056d6c9d65fcbedb84a47fcb
Lucas Loegel <lucas.loegel@outlook.com>
2017-11-01 15:18:37 +0100

    Add more tests

---
+++ src/CSharpDiscriminatedUnion.Generation.Tests/Struct/StructSingleCaseWithMultipleParametersTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Reflection;
+using CSharpDiscriminatedUnion.Generation.Tests.UnionTypes;
+using NUnit.Framework;
+
+namespace CSharpDiscriminatedUnion.Generation.Tests.Struct
+{
+    public class StructSingleCaseWithMultipleParametersTests
+    {
+        [Test]
+        public void HasCaseMethod()
+        {
+            var caseMethods = typeof(StructBook).GetMethods(BindingFlags.Public | BindingFlags.Static)
+                                                     .Where(m => m.Name == "NewBook")
+                                                     .ToArray();
+            //assert
+            Assert.That(caseMethods, Has.Exactly(1).Items);
+            var singleMethod = caseMethods[0];
+            Assert.That(singleMethod, Has.Property(nameof(singleMethod.ReturnType)).EqualTo(typeof(StructBook)));
+            Assert.That(singleMethod.GetParameters().Select(p => p.ParameterType), Is.EquivalentTo(new[] { typeof(string), typeof(int), typeof(string) }));
+            Assert.That(singleMethod.GetParameters().Select(p => p.Name), Is.EquivalentTo(new[] { "author", "pageCount", "title" }));
+        }
+    }
+}
